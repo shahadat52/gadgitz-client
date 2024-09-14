4d84e8b6ad1d07e37d4b2cdf71e818af382116b7
shahadat52 <shahadathossain.sh255@gmail.com>
2024-09-14 09:02:38 +0600

    product sort filter design added

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -6,12 +6,22 @@ const ProductsPage = async () => {
     const data = await res.json();
     const products = data?.data
     return (
-        <div>
-            <h1 className='text-center font-bold text-4xl my-5'>Our Products</h1> <hr />
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 p-3'>
-                {
-                    products?.map((item: any) => <ProductCard key={item._id} product={item} />)
-                }
+        <div className='grid grid-cols-4'>
+            <div className='col-span-1'>
+                <p>Sort By</p>
+
+            </div>
+            <div className='col-span-3'>
+                <h1 className='text-center font-bold text-4xl my-5'>Our Products</h1> <hr />
+               <div>
+               <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+               
+               </div>
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 p-3'>
+                    {
+                        products?.map((item: any) => <ProductCard key={item._id} product={item} />)
+                    }
+                </div>
             </div>
         </div>
     );
